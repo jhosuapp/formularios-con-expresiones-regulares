bc7b983a99d19246d36bf2f9d5843c491598e704
jhosuapp <jhosuapvll@gmail.com>
2022-12-24 15:00:52 -0800

    create vite config with libraries and custom structuring

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'sources/js/app.js'),
+      name: 'Mainscript',
+      fileName: 'js/mainscript'
+    },
+    devtool: 'source-map',
+    watch: true,
+    target: "es2015",
+    rollupOptions: {
+      output: {
+        assetFileNames: "css/main.css",
+      },
+    },
+  },
+});
